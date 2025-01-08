0bca8553bdb785769aa2c63f246868001307dc52
lvbukai86 <lvchengda@1099098162705055.onaliyun.com>
2025-01-08 19:51:57 +0800

    提交1

--- src/routes/IndexPage.js
+++ src/routes/IndexPage.js
@@ -8,7 +8,7 @@ function IndexPage() {
       <h1 className={styles.title}>Yay! Welcome to dva2222!</h1>
       <div className={styles.welcome} />
       <ul className={styles.list}>
-        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+        <li>To get started, edit22222 <code>src/index.js</code> and save to reload.</li>
         <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
       </ul>
     </div>
